4338fb353445a312ebd4a1d817e52ce0f050ca57
Daan Krol <daankrol@icloud.com>
2021-06-13 15:15:59 +0200

    fixed buttons being to small. Set rounds to 5. Fixed kripke model viewer where only the true world should be shown

--- game_model.py
+++ game_model.py
@@ -12,7 +12,7 @@ from public_announcement import PublicAnnouncement, AnnouncementType
 class GameModel:
     def __init__(self):
         # TODO determine rounds/cards per round
-        self.cards_per_round = [4, 4]
+        self.cards_per_round = [3, 4, 5, 4, 3]
         self.players = [GreedyKripkePlayer(i, Seat(i)) for i in range(4)]
         for p in self.players:
             p.set_game_model(self)
